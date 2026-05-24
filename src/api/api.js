e8528bd4da551e93496f69be401c3fbaf375ebd5
Asiedu Noble Kwadwo <148404045+kwadwonoble@users.noreply.github.com>
2026-05-24 11:59:21 +0000

    Fixed frontend production API

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
 
   baseURL:
-    "https://teacher-attendance-system-1-qgf0.onrender.com/api",
+    "https://teacher-attendance-system-1-qgf0.onrender.com",
 
 });
 
