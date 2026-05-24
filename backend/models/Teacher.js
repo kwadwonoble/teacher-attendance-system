2ef046664e76d6d33bc2be9082dcd2b364f2245c
Asiedu Noble Kwadwo <148404045+kwadwonoble@users.noreply.github.com>
2026-05-24 17:07:19 +0000

    Added jhsSubject field

--- backend/models/Teacher.js
+++ backend/models/Teacher.js
@@ -1,45 +1,49 @@
 const mongoose = require("mongoose");
 
-const teacherSchema = new mongoose.Schema({
+const teacherSchema = new mongoose.Schema(
 
-  firstName: {
-    type: String,
-    required: true,
-    trim: true,
+  {
+
+    firstName: {
+      type: String,
+      required: true,
+    },
+
+    lastName: {
+      type: String,
+      required: true,
+    },
+
+    subject: {
+      type: String,
+      required: true,
+    },
+
+    jhsSubject: {
+      type: String,
+      default: "",
+    },
+
+    phone: {
+      type: String,
+      required: true,
+    },
+
+    town: {
+      type: String,
+      required: true,
+    },
+
   },
 
-  lastName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
+  {
+    timestamps: true,
+  }
 
-  subject: {
-    type: String,
-    required: true,
-  },
-
-
-  jhsSubject: {
-  type: String,
-},
-
-  phone: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-
-  town: {
-    type: String,
-    required: true,
-  },
-
-}, {
-
-  timestamps: true,
-
-});
+);
 
 module.exports =
-  mongoose.model("Teacher", teacherSchema);
+  mongoose.model(
+    "Teacher",
+    teacherSchema
+  );
